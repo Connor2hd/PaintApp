63b7141416328a3f3e2a3bfe450844be05eef909
Connor Mackay <Connor@goliathhosting.com>
2022-05-14 15:15:51 -0400

    Adding the API project
    The API project was initially it's own repo so it was missed in the initial commit.

---
+++ PaintAppAPI/PaintAppAPI/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using PaintAppModels;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddDbContext<PaintContext>(opt => opt.UseInMemoryDatabase("Paint"));
+builder.Services.AddDbContext<PaintContext>(opt => opt.UseSqlServer("name=ConnectionStrings:Paint"));
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
